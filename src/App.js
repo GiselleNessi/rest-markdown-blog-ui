01accaf6a0106622e650705b3c1fd5b66f4f4c15
Giselle Chacon <gisecn@gmail.com>
2020-10-20 19:04:39 +0100

    Update view

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
 import {Router, Route, Switch} from 'react-router-dom';
-import {createBrowserHistory} from "history";
-
+import { history } from "./helpers";
 import Layout from './containers/Layout'
 import PostList from './containers/PostList'
 import PostDetail from './containers/PostDetail'
 import PostCreate from './containers/PostCreate'
 import PostUpdate from './containers/PostUpdate'
 import PostDelete from './containers/PostDelete'
-
-const history = createBrowserHistory();
 
 function App() {
   return (
@@ -19,7 +16,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={PostList} />
           <Route path="/create" component={PostCreate} />
-          <Route path="/posts/:postSlug" component={PostDetail} />
+          <Route exact path="/posts/:postSlug" component={PostDetail} />
           <Route path="/posts/:postSlug/update" component={PostUpdate} />
           <Route path="/posts/:postSlug/delete" component={PostDelete} />
         </Switch>
